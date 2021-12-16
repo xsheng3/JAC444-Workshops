61df66fa0bcc3353cee9464e9032353d8d9fee56
xsheng3 <71341820+xsheng3@users.noreply.github.com>
2021-12-16 11:13:53 -0500

    Add files via upload

---
+++ Workshop #4/interfaces/Payable.java
@@ -0,0 +1,16 @@
+/**********************************************
+Workshop #04
+Course: JAC444
+Last Name: Sheng
+First Name: Xuewei
+ID:061745121
+Section: NFF
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Signature Xuewei Sheng
+Date: Oct 14, 2021
+**********************************************/
+package jac444.ws4.interfaces;
+
+public interface Payable {
+	double getPaymentAmount();
+}

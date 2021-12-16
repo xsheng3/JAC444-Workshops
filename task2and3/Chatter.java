82087d095ffc20985bcd7b12bc1220aadf05d03c
xsheng3 <71341820+xsheng3@users.noreply.github.com>
2021-12-16 11:06:08 -0500

    Add files via upload

---
+++ task2and3/Chatter.java
@@ -0,0 +1,25 @@
+/**********************************************
+Workshop #01
+Course: JAC444
+Last Name: Sheng
+First Name: Xuewei
+ID:061745121
+Section: NFF
+This assignment represents my own work in accordance with Seneca Academic Policy.
+Signature Xuewei Sheng
+Date: Sep 25, 2021
+**********************************************/
+package task2and3;
+
+public class Chatter {
+private String[] chats = {
+		"Oh, you’re going for broke, huh?",
+		"Aw c’mon, take a chance!",
+		"You’re up big. Now’s the time to cash in your chips!",
+		"Come on, you can make it!"		
+};
+public void generateString() {
+	int idx = (int)(Math.random() * chats.length);
+	System.out.print("\n" + chats[idx] + "\n\n");
+}
+}

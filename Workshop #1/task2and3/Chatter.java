a446612b2458c00a12a0a078d91a8b1d3ee45dec
xsheng3 <71341820+xsheng3@users.noreply.github.com>
2021-12-16 11:07:44 -0500

    Update Chatter.java

--- Workshop #1/task2and3/Chatter.java
+++ Workshop #1/task2and3/Chatter.java
@@ -3,10 +3,6 @@ Workshop #01
 Course: JAC444
 Last Name: Sheng
 First Name: Xuewei
-ID:061745121
-Section: NFF
-This assignment represents my own work in accordance with Seneca Academic Policy.
-Signature Xuewei Sheng
 Date: Sep 25, 2021
 **********************************************/
 package task2and3;
